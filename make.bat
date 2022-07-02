892b3e761ada14a36cef6f5b8c6c1538778312eb
joonas <joonas1000@gmail.com>
2022-07-02 18:57:09 +0300

    link to allegro modules instead of monolith

--- make.bat
+++ make.bat
@@ -9,4 +9,10 @@ echo #define BUILD_DATE "%date%, time %time%" >> %file%
 echo #endif >> %file%
 
 @echo on
-gcc -static-libstdc++ pomppu.c allegro_compat.c synth.c music.c -Iinclude liballegro_monolith.dll.a -O3 -o crystal
+gcc -static-libstdc++ pomppu.c allegro_compat.c synth.c music.c -Iinclude ^
+ liballegro_audio.dll.a ^
+ liballegro_font.dll.a ^
+ liballegro_image.dll.a ^
+ liballegro_primitives.dll.a ^
+ liballegro.dll.a ^
+ -O3 -o crystal
